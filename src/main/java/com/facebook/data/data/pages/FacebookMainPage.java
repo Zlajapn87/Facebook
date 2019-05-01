4bf317bea673deabd961ed25935ac2469af3af0e
Zlajapn87 <zlatkovic.nikola1987@gmail.com>
2019-05-01 11:38:35 +0200

    Adding SignUp Test (Part 1)

--- src/main/java/com/facebook/data/data/pages/FacebookMainPage.java
+++ src/main/java/com/facebook/data/data/pages/FacebookMainPage.java
@@ -18,6 +18,10 @@ public class FacebookMainPage {
    @FindBy(xpath = "//input[@id='email']") WebElement  field_EmailLogin;
    @FindBy(xpath = "//input[@id='pass']") WebElement  field_PasswordLogin;
    public  @FindBy(xpath = "//input[@value='Log In']") WebElement  button_Login;
+   @FindBy(xpath="//input[@name='firstname']") WebElement field_FirstName;
+   @FindBy(xpath="//input[@name='lastname']") WebElement field_LastName;
+   @FindBy(xpath="//input[@name='reg_email__']") WebElement field_Mobile;
+   @FindBy(xpath="//input[@name='reg_passwd__']") WebElement field_NewPassword;
 
 
     //Selectors of the elements located on the FaceBook Home Page
@@ -31,20 +35,43 @@ public class FacebookMainPage {
   }
 
   //Actions
-    public void setTtext_EmailLogin (String text){
-     field_EmailLogin.sendKeys(text);
-     Assert.assertEquals(field_EmailLogin.getAttribute("value"), text);
+    public void setTtext_EmailLogin (String text) {
+        field_EmailLogin.sendKeys(text);
+        Assert.assertEquals(field_EmailLogin.getAttribute("value"), text);
+    }
+     public void setTtext_PasswordLogin (String text){
+         field_PasswordLogin.sendKeys(text);
+         Assert.assertEquals(field_PasswordLogin.getAttribute("value"), text);
 
     }
 
-    public void setTtext_PasswordLogin (String text){
-       field_PasswordLogin.sendKeys(text);
-        Assert.assertEquals(field_PasswordLogin.getAttribute("value"), text);
+    public void setTtext_FirstNameField(String text) {
+        field_FirstName.sendKeys(text);
+        Assert.assertEquals(field_FirstName.getAttribute("value"), text);
     }
 
-    public void clickOnButton(WebElement element){
-      element.click();
+     public void setTtext_LastNameField (String text){
+        field_PasswordLogin.sendKeys(text);
+        Assert.assertEquals(field_LastName.getAttribute("value"), text);
+        }
+
+     public void setTtext_fieldMobile (String text){
+        field_PasswordLogin.sendKeys(text);
+        Assert.assertEquals(field_Mobile.getAttribute("value"), text);
+        }
+
+    public void setTtext_selectBirthday (String text){
+        field_PasswordLogin.sendKeys(text);
+        Assert.assertEquals(field_Mobile.getAttribute("value"), text);
     }
+
+    public void setTtext_fieldPassword (String text){
+        field_PasswordLogin.sendKeys(text);
+        Assert.assertEquals(field_NewPassword.getAttribute("value"), text);
+    }
+        public void clickOnButton (WebElement element){
+        element.click();
+        }
 
 
 
---
+++ src/test/java/com/facebook/tests/SignUp_Test.java
@@ -0,0 +1,46 @@
+package com.facebook.tests;
+
+import com.facebook.data.data.pages.FacebookMainPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SignUp_Test {
+
+    public WebDriver driver;
+    FacebookMainPage fbMainPage;
+
+
+    @BeforeClass(alwaysRun=true)
+    public void setup(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\zlatk\\OneDrive\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver(); //Driver has to be created before the page.
+        fbMainPage = PageFactory.initElements(driver, FacebookMainPage.class); //page factory
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test(groups = {"p1"})
+    public void testSignUpMainPage(){
+        driver.manage().deleteAllCookies();
+
+        driver.get(fbMainPage.PAGE_URL);
+        assertTrue(driver.getTitle().contains(fbMainPage.PAGE_TITLE));
+        fbMainPage.setTtext_FirstNameField("QA");
+        fbMainPage.setTtext_LastNameField("Zlaja");
+        fbMainPage.setTtext_fieldMobile("validEmail@facebook.com");
+        fbMainPage.setTtext_fieldPassword("password");
+    }
+
+}
